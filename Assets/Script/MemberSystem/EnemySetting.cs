d65ec829b93ab022f6f11cfb18ac8ebbead12d1e
OzuAkira <22311038ao@tama.ac.jp>
2025-11-30 14:48:23 +0900

    EnemySetting途中

--- Assets/Script/MemberSystem/EnemySetting.cs
+++ Assets/Script/MemberSystem/EnemySetting.cs
@@ -1,6 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySetting : MonoBehaviour
 {
-    
+    [SerializeField] string[] keies;
+    [SerializeField] GameObject[] windowObj;
+
+    public Dictionary<string,GameObject[]> enemyWindow;
+
+    void Start()
+    {
+        for(int i = 0; i < keies.Length; i++)
+        {
+            GameObject[] oneListObj = {gameObject};
+            enemyWindow.Add(keies[i],oneListObj);
+        }
+    }
 }
